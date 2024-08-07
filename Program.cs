a2c6ab9f7cdeb6a34523a08ab25b2ea9b3c2d25b
unknown <legiathinh0508@gmail.com>
2024-08-07 18:05:01 +0700

    ChuyenDoiTienTe

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChuyenDoiTienTe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double rate = 23000; // Tỉ giá 1 USD = 23000 VND
+            
+           
+            Console.WriteLine("Enter the amount in USD: ");
+            double usdValue = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine($"The equivalent amount in VND is: {usdValue * rate}");
+            double vndValue = usdValue * rate;
+            Console.WriteLine($"The equivalent amount in VND is: {vndValue}");
+        }
+    }
+}
